8803ba556bee7960a113e8a3b6b020967fae4b1f
Heiya <morooka.akira.inon29@gmail.com>
2025-07-18 08:14:29 +0900

    feat: Update aicm to v0.4.0

--- Formula/aicm.rb
+++ Formula/aicm.rb
@@ -5,29 +5,29 @@ class Aicm < Formula
 
   on_macos do
     on_arm do
-      url     "https://github.com/morooka-akira/aicm/releases/download/v0.3.0/aicm-aarch64-apple-darwin",
+      url     "https://github.com/morooka-akira/aicm/releases/download/v0.4.0/aicm-aarch64-apple-darwin",
               using: :nounzip
-      sha256  "0fbe9552ce455b68317a01e5f8779174a872bd5d1e8027d929f330346107f253"
+      sha256  "869349a83f60761cf717347fef2e7cd99913f02994f199adc2a41587e17d7341"
     end
 
     on_intel do
-      url     "https://github.com/morooka-akira/aicm/releases/download/v0.3.0/aicm-x86_64-apple-darwin",
+      url     "https://github.com/morooka-akira/aicm/releases/download/v0.4.0/aicm-x86_64-apple-darwin",
               using: :nounzip
-      sha256  "2047235b9309b5d5db86f984365ea51f887479f0e0c853e4b8058ea60d37eec1"
+      sha256  "c3525da43fa3a532c5229576c94ab909bf0f2a798e2cca7cf7b7ff697d79ea31"
     end
   end
 
   on_linux do
     on_intel do
-      url     "https://github.com/morooka-akira/aicm/releases/download/v0.3.0/aicm-x86_64-unknown-linux-gnu",
+      url     "https://github.com/morooka-akira/aicm/releases/download/v0.4.0/aicm-x86_64-unknown-linux-gnu",
               using: :nounzip
-      sha256  "f1960ad87154baada8dffddbb3f804df79a03e068739bdeda938c1b665ef0245"
+      sha256  "23a011a2ec857b1fb34633648d824765558a926204ebd115e07f52a788b13e72"
     end
 
     on_arm do
-      url     "https://github.com/morooka-akira/aicm/releases/download/v0.3.0/aicm-aarch64-unknown-linux-gnu",
+      url     "https://github.com/morooka-akira/aicm/releases/download/v0.4.0/aicm-aarch64-unknown-linux-gnu",
               using: :nounzip
-      sha256  "a6f3eedeb5e644527e05c32d3500b9bf937c3ee6beeabece715b638b09c6a47b"
+      sha256  "f85ca6b51751c154cb36e8d38b91191975254196db7bb2166a6cd6772782a8af"
     end
   end
 
@@ -38,6 +38,6 @@ class Aicm < Formula
   end
 
   test do
-    assert_match "0.3.0", shell_output("#{bin}/aicm --version")
+    assert_match "0.4.0", shell_output("#{bin}/aicm --version")
   end
 end
